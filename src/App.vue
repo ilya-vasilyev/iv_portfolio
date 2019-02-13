49bf7eb817354bb2d8eac86ea1d78178e95bafcd
Ilya <hardcor4eg@gmail.com>
2019-02-13 11:08:53 +0300

    add more meta info

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,46 @@
 
 <script>
 export default {
-  metaInfo: {
-    title: 'Default Title',
-    titleTemplate: '%s | My Awesome Webapp',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-    ]
+  data () {
+    return {
+      title: 'Ilya Vasilyev | homepage',
+      description: 'personal web page of Ilya Vasilyev',
+      url: 'https://www.my-site.com/my-special-page',
+      image: 'https://www.my-site.com/my-special-image.jpg'
+    }
+  },
+  metaInfo () {
+    return {
+      title: 'homepage',
+      titleTemplate: 'Ilya Vasilyev | %s',
+      links: [
+        { rel: 'canonical', href: this.url }
+      ],
+      meta: [
+        { 'http-equiv': 'Content-Type', content: 'text/html; charset=utf-8' },
+
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: this.description },
+
+        { property: 'og:title', content: this.title },
+        { property: 'og:site_name', content: 'Ilya Vasilyev' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:url', content: this.url },
+        { property: 'og:image', content: this.image },
+        { property: 'og:description', content: this.description },
+
+        { name: 'twitter:card', content: 'summary' },
+        { name: 'twitter:site', content: this.url },
+        { name: 'twitter:title', content: this.title },
+        { name: 'twitter:description', content: this.description },
+        { name: 'twitter:creator', content: 'Ilya Vasilyev' },
+        { name: 'twitter:image:src', content: this.image },
+
+        { itemprop: 'name', content: this.title },
+        { itemprop: 'description', content: this.description },
+        { itemprop: 'image', content: this.image }
+      ]
+    }
   }
 }
 </script>
@@ -23,7 +56,7 @@ export default {
 <style lang="scss">
 
 a {
-  color: #bed;
+  color: #444;
 }
 
 </style>
--- src/views/A.vue
+++ src/views/A.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="a">
+    <div
+      itemscope
+      itemtype="http://schema.org/Person">
+      hi, my name is <span itemprop="name">Ilya</span> and I'm a <span itemprop="disambiguatingDescription">frontend web developer</span>
+    </div>
     <h1 :class="{ rotate: true }">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h1>
     <p>Worem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     <button @click="loadedChart = true">load chart</button>
