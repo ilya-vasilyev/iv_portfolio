594a89bd980fb3758215163cba32a05ef25e18b6
Ilya <hardcor4eg@gmail.com>
2019-02-18 07:57:36 +0300

    add anime-js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import Vue from 'vue'
 import Meta from 'vue-meta'
 import router from './router'
 import store from './store'
+import VueAnime from './vue-anime'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
 Vue.use(Meta)
+Vue.use(VueAnime)
 
 /* eslint-disable no-new */
 new Vue({
