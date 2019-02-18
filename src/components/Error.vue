769f2ffc1d05406bcb6f1ec8100e0660aff4eeb6
Ilya <hardcor4eg@gmail.com>
2019-02-18 08:07:40 +0300

    add Error & Loading components

---
+++ src/components/Error.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="component-error">
+    <p>can't load component...</p>
+    <h3>¯\_(ツ)_/¯</h3>
+  </div>
+</template>
+
+<style lang="scss">
+.component-loading,
+.component-error  {
+  align-items: center;
+  background: #fafafa;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 80%;
+  margin: 40px auto;
+  height: 400px;
+  color: #ccc;
+  * {
+    margin: 1rem;
+  }
+}
+</style>
---
+++ src/components/Loading.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="component-loading">
+    <p>loading...</p>
+  </div>
+</template>
