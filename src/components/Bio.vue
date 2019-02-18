0fb6ce2ab144bb21e916f6e734ad48088200708f
Ilya <hardcor4eg@gmail.com>
2019-02-18 08:07:08 +0300

    add Bio component

---
+++ src/components/Bio.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="bio">
+    <div>
+      <h1>ILYA VASILYEV</h1>
+      <span>\/</span>
+    </div>
+    <div
+      itemscope
+      itemtype="http://schema.org/Person">
+      hi, my name is
+      <span itemprop="name">Ilya</span>
+      and I'm a
+      <span itemprop="disambiguatingDescription">frontend web developer</span>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.bio header {
+  display: flex;
+  align-items: center;
+}
+</style>
