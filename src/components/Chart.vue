b9c58bdb8407c23e04f8eb8ad8b7de80aab9871f
Ilya <hardcor4eg@gmail.com>
2019-02-20 17:45:16 +0300

    rebuilding chart

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="">
-    <button @click="updateSettings({ type: 'bar', area: '', field: 'rating' })">
-      ratings
+    <button @click="loadBar('rating')">
+      rating
     </button>
-    <button @click="updateSettings({ type: 'donut', area: 'front', field: 'years' })">
+    <button @click="loadBar('years')">
       years
     </button>
-    <button @click="updateSettings({ type: 'scatter', area: '', field: 'level' })">
+    <button @click="loadBar('level')">
       level
+    </button>
+    <button @click="loadYearsLevel()">
+      loadYearsLevel
     </button>
     <div id="chart" />
   </div>
@@ -25,83 +28,160 @@ export default {
   },
   data () {
     return {
-      settings: {
-        field: '',
-        area: '',
-        type: 'bar'
-      }
+      bbDefaults: {
+        bindto: '#chart',
+        bar: { width: { ratio: 0.8, max: 100 } },
+        tooltip: { show: false },
+        transition: { duration: 0 },
+        legend: { show: false },
+        color: { pattern: ['#444'] }
+      },
+      sortBy: 'level',
+      showNew: false,
+      showDisgrace: false
+    }
+  },
+  computed: {
+    oneDimensionData () {
+      let result = {}
+      let skills = this.$store.state.skills
+      skills
+        // .filter(skill => skill.isNew === this.showNew && skill.isDisgrace === this.showDisgrace)
+        .sort((a, b) => b[this.sortBy] - a[this.sortBy])
+        .forEach(skill => {
+          Object.entries(skill).forEach(([key, value]) => {
+            if (!result[key]) result[key] = []
+            result[key].push(value)
+          })
+        })
+      return result
     }
   },
   mounted () {
-    this.$options.chart = bb.generate({
-      bindto: '#chart',
-      data: {
-        x: 'name',
-        type: this.settings.type,
-        rows: [],
-        labels: true
-      },
-      axis: {
-        rotated: true,
-        x: { type: 'category' },
-        y: { show: false }
-      },
-      tooltip: { show: false },
-      legend: { show: false },
-      color: { pattern: ['#444'] },
-      transition: { duration: 0 }
-    })
-    this.updateSettings({ field: 'years' })
+    this.$options.chart = { destroy: () => {} }
+    this.loadBar('years')
   },
   methods: {
-    format (fields, area) {
-      let result = [fields]
-      this.data
-        .filter(el => el.area === area || !area)
-        .sort((a, b) => b[fields[1]] - a[fields[1]])
-        .forEach(item => {
-          result.push(
-            fields.map(field => item[field])
-          )
-        })
-      return result
-    },
-    updateSettings ({ field = this.settings.field, area = this.settings.area, type = this.settings.type }) {
+    hideChart (callback) {
       this.$anime({
-        targets: '#chart svg',
-        scaleY: 1.4,
-        opacity: 0,
+        targets: '#chart',
+        scaleY: 0.4,
+        opacity: 0.5,
         filter: 'blur(10px)',
         duration: 300,
         easing: 'easeInBack',
-        complete: unload.bind(this)
+        complete: callback.bind(this)
       })
-      function unload () {
-        this.$options.chart.unload({
-          done: load.bind(this)
+    },
+    showChart () {
+      this.$anime({
+        targets: '#chart',
+        scaleY: 1,
+        opacity: 1,
+        filter: 'blur(0px)',
+        duration: 200,
+        easing: 'easeOutBack'
+      })
+    },
+    showBars () {
+      let staggerDelay = 0
+      let animationKeyframes = [
+        {
+          value: 0,
+          duration: 0
+        }, {
+          value: 1,
+          duration: 250,
+          delay: this.$anime.stagger(50, { start: staggerDelay }),
+          easing: 'easeOutQuad'
+        }
+      ]
+      this.$anime({
+        targets: '#chart .bb-chart-bars .bb-bar',
+        scaleX: animationKeyframes,
+        opacity: animationKeyframes
+      })
+
+      staggerDelay = 200
+
+      this.$anime({
+        targets: '#chart .bb-chart-texts .bb-text',
+        opacity: animationKeyframes
+
+      })
+    },
+
+    loadBar (field) {
+      this.hideChart(rebuild)
+      function rebuild () {
+        this.sortBy = field
+        this.$options.chart.destroy()
+        this.$options.chart = bb.generate({
+          ...this.bbDefaults,
+          data: {
+            type: 'bar',
+            columns: [
+              [ field, ...this.oneDimensionData[field] ]
+            ],
+            labels: true
+          },
+          axis: {
+            rotated: true,
+            x: {
+              type: 'category',
+              categories: this.oneDimensionData.name
+            },
+            y: { show: false }
+          }
         })
+        this.showChart()
+        this.showBars()
       }
-      function load () {
-        this.$options.chart.load({
-          rows: this.format(['name', field], area),
-          done: transform.bind(this)
+    },
+
+    loadYearsLevel () {
+      this.hideChart(rebuild)
+      function rebuild () {
+        this.$options.chart.destroy()
+        this.$options.chart = bb.generate({
+          ...this.bbDefaults,
+          data: {
+            type: 'scatter',
+            columns: [
+              ['vue_level', 8],
+              ['vue', 4],
+              ['docker_level', 2],
+              ['docker', 1]
+            ],
+            xs: {
+              'vue': 'vue_level',
+              'docker': 'docker_level'
+            },
+            labels: {
+              format: (v, id) => id
+            }
+          },
+          axis: {
+            x: {
+              label: 'level',
+              min: 1,
+              max: 10
+            },
+            y: {
+              label: 'years',
+              min: 1,
+              max: 10
+            }
+          },
+          grid: {
+            x: { show: true },
+            y: { show: true }
+          }
         })
-      }
-      function transform () {
-        this.$options.chart.transform(type)
-        this.settings.field = field
-        this.settings.area = area
-        this.settings.type = type
-        this.$anime({
-          targets: '#chart svg',
-          scaleY: 1,
-          opacity: 1,
-          filter: 'blur(0px)',
-          duration: 200,
-          easing: 'easeOutBack'
-        })
+        this.showChart()
       }
     }
+
   }
 }
 </script>
