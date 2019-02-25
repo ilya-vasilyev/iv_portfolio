754420bbef15017b702beea75c171cfcc153a5fe
Ilya <hardcor4eg@gmail.com>
2019-02-25 18:49:11 +0300

    add real table

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,55 +1,135 @@
 <template>
-  <div ref="table"/>
+  <section>
+    <div ref="table" />
+    <label for="tableShowNew">tableShowNew</label>
+    <input
+      id="tableShowNew"
+      type="checkbox"
+      :value="filters.isNew"
+      @change="toggleFilter('isNew', !filters.isNew)"
+    >
+    <label for="tableShowDisgrace">tableShowDisgrace</label>
+    <input
+      id="tableShowDisgrace"
+      type="checkbox"
+      :value="filters.isDisgrace"
+      @change="toggleFilter('isDisgrace', !filters.isDisgrace)"
+    >
+  </section>
 </template>
 
 <script>
 import Tabulator from 'tabulator-tables'
 
 export default {
-  name: 'Chart',
+  name: 'Table',
   data: function () {
     return {
-      tabulator: null,
-      tableData: [
-        {id:1, name:"Oli Bob", age:"12", col:"red", dob:""},
-        {id:2, name:"Mary May", age:"1", col:"blue", dob:"14/05/1982"},
-        {id:3, name:"Christine Lobowski", age:"42", col:"green", dob:"22/05/1982"},
-        {id:4, name:"Brendon Philips", age:"125", col:"orange", dob:"01/08/1980"},
-        {id:5, name:"Margret Marmajuke", age:"16", col:"yellow", dob:"31/01/1999"}
+      filters: {
+        isNew: false,
+        isDisgrace: false
+      }
+    }
+  },
+  mounted () {
+    this.$options.tabulator = new Tabulator(this.$refs.table, {
+      layout: 'fitColumns',
+      resizableColumns: 'header',
+      columnMinWidth: 100,
+      data: this.$store.state.skills,
+      initialFilter: [
+        { field: 'isNew', type: '!=', value: true },
+        { field: 'isDisgrace', type: '!=', value: true }
       ],
-    }
-  },
-  watch:{
-    tableData:{
-      handler: function (newData) {
-        this.tabulator.replaceData(newData);
-      },
-      deep: true,
-    }
-  },
-  mounted() {
-    this.tabulator = new Tabulator(this.$refs.table, {
-      data: this.tableData,
-      reactiveData:true,
+      initialSort: [
+        { column: 'level', dir: 'desc' }
+      ],
       columns: [
-        { title: "Name", field: "name", width: 150 },
-        { title: "Age", field: "age", align: "left", formatter: "progress" },
-        { title: "Favourite Color", field: "col" },
-        { title: "Date Of Birth", field: "dob", align: "center" },
-        { title: "Rating", field: "rating", align: "center", formatter: "star" },
-        { title: "Passed?", field: "passed", align: "center", formatter: "tickCross" }
+        { title: 'Name', field: 'name', minWidth: 150, widthGrow: 2 },
+
+        { title: 'years', field: 'years', align: 'right' },
+
+        { title: 'level',
+          field: 'level',
+          formatter: 'progress',
+          formatterParams: {
+            min: 0,
+            max: 10,
+            color: '#444'
+          }
+        },
+
+        { title: 'rating',
+          field: 'rating',
+          formatter: 'progress',
+          formatterParams: {
+            min: 0,
+            max: 10,
+            color: '#444'
+          }
+        },
+
+        { title: 'area', field: 'area' },
+
+        { title: 'is New',
+          field: 'isNew',
+          visible: false,
+          formatter: 'tickCross',
+          formatterParams: {
+            allowEmpty: false,
+            allowTruthy: true
+          }
+        },
+
+        { title: 'is Disgrace',
+          field: 'isDisgrace',
+          visible: false,
+          formatter: 'tickCross',
+          formatterParams: {
+          }
+        }
       ]
-    });
+    })
+    window.addEventListener('resize', () => this.$options.tabulator.redraw())
+  },
+  methods: {
+    toggleFilter (filter, value) {
+      this.filters[filter] = value
+      this.$options.tabulator.clearFilter()
+      Object.entries(this.filters).forEach(([field, val]) => {
+        if (!val) this.$options.tabulator.addFilter(field, '!=', true)
+      })
+      // if (value) {
+      //
+      // } else {
+      //   this.$options.tabulator.setFilter(value, '!=', true)
+      // }
+    },
+    toggleDisgrace () {
+
+    }
   }
 }
 </script>
 
 <style lang="scss">
 
-@import 'tabulator-tables/dist/css/tabulator_simple.min.css';
+@import 'tabulator-tables/dist/css/bulma/tabulator_bulma.min.css';
 
-.bb-line {
-  stroke-width: 33px;
+.tabulator {
+  width: 100%;
+  max-width: 600px;
+  min-height: 500px;
+  margin: 0 auto;
+  background: transparent;
+  color: currentColor;
+
+}
+.tabulator-responsive-collapse {
+  margin-bottom: 30px;
+}
+.tabulator-row .tabulator-cell,
+.tabulator-header .tabulator-col .tabulator-col-content {
 }
 
 </style>
