849956c0fe1aa7113561089c1bef1b08fb720942
Ilya <ilya.vasilyev@gmx.com>
2019-03-24 12:42:12 +0300

    add reloading to error component

--- src/components/Error.vue
+++ src/components/Error.vue
@@ -2,5 +2,16 @@
   <div class="component-error">
     <span>can't load component...</span>
     <h3>¯\_(ツ)_/¯</h3>
+    <button @click="reload()">Reload page</button>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    reload () {
+      window.location.reload()
+    }
+  }
+}
+</script>
